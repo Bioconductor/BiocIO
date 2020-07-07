7de98eeb227efdfccafd217a0331f4defbb91bf6
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2020-07-07 12:59:50 -0400

    Added utils tests

---
+++ tests/testthat/test_utils.R
@@ -0,0 +1,29 @@
+test_that("Test .parseURI", {
+    uri <- 'http://foo.com/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "http", path = "/bar.txt"))
+    
+    uri <- 'file:////foo/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "file", path = "//foo/bar.txt"))
+
+    uri <- 'file:///foo/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "file", path = "/foo/bar.txt"))
+
+    uri <- 'https://foo.com/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "https", path = "/bar.txt"))
+
+    uri <- 'ftp://foo.com/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "ftp", path = "/bar.txt"))
+
+    uri <- 'smtp://foo.com/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "smtp", path = "/bar.txt"))
+
+    uri <- 'file:///C:/foo/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(parsed, list(scheme = "file", path = "/C:/foo/bar.txt"))
+})
