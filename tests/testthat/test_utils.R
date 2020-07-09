f2e77c626eada23f428b947c445fd3539b29376a
Daniel Van Twisk <daniel.vantwisk@roswellpark.org>
2020-07-09 13:00:52 -0400

    Test fixes

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,36 +1,32 @@
 test_that("Test .parseURI", {
     uri <- 'http://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
     expect_equal(parsed, list(scheme = "http", path = "/bar.txt"))
     
     uri <- 'file:////foo/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
+    warining(parsed)
     expect_equal(parsed, list(scheme = "file", path = "//foo/bar.txt"))
 
     uri <- 'file:///foo/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
+    warining(parsed)
     expect_equal(parsed, list(scheme = "file", path = "/foo/bar.txt"))
 
     uri <- 'https://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
     expect_equal(parsed, list(scheme = "https", path = "/bar.txt"))
 
     uri <- 'ftp://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
     expect_equal(parsed, list(scheme = "ftp", path = "/bar.txt"))
 
     uri <- 'smtp://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
     expect_equal(parsed, list(scheme = "smtp", path = "/bar.txt"))
 
     uri <- 'file:///C:/foo/bar.txt'
     parsed <- .parseURI(uri)
-    parsed$path <- gsub("\\\\", "/", parsed$path)
+    warining(parsed)
     expect_equal(parsed, list(scheme = "file", path = "/C:/foo/bar.txt"))
 })
