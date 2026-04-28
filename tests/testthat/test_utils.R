d10dcb920e5577d132d9c03d1b30bb75b2e28e15
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-04-28 18:14:42 -0400

    add hosts in unit tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -1,8 +1,18 @@
 test_that("Test .parseURI", {
     uri <- 'http://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    expect_equal(parsed, list(scheme = "http", path = "/bar.txt"))
-    
+    expect_equal(
+        parsed,
+        list(scheme = "http", host = "foo.com", path = "/bar.txt")
+    )
+
+    uri <- 'http://www.example.com/foo/bar.txt'
+    parsed <- .parseURI(uri)
+    expect_equal(
+        parsed,
+        list(scheme = "http", host = "www.example.com", path = "/foo/bar.txt")
+    )
+
     uri <- 'file:////foo/bar.txt'
     parsed <- .parseURI(uri)
     expect_equal(parsed, list(scheme = "file", path = "//foo/bar.txt"))
@@ -13,17 +23,29 @@ test_that("Test .parseURI", {
 
     uri <- 'https://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    expect_equal(parsed, list(scheme = "https", path = "/bar.txt"))
+    expect_equal(
+        parsed,
+        list(scheme = "https", host = "foo.com", path = "/bar.txt")
+    )
 
     uri <- 'ftp://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    expect_equal(parsed, list(scheme = "ftp", path = "/bar.txt"))
+    expect_equal(
+        parsed,
+        list(scheme = "ftp", host = "foo.com", path = "/bar.txt")
+    )
 
     uri <- 'smtp://foo.com/bar.txt'
     parsed <- .parseURI(uri)
-    expect_equal(parsed, list(scheme = "smtp", path = "/bar.txt"))
+    expect_equal(
+        parsed,
+        list(scheme = "smtp", host = "foo.com", path = "/bar.txt")
+    )
 
     uri <- 'file://C:/foo/bar.txt'
     parsed <- .parseURI(uri)
-    expect_equal(parsed, list(scheme = "file", path = "C:/foo/bar.txt"))
+    expect_equal(
+        parsed,
+        list(scheme = "file", path = "C:/foo/bar.txt")
+    )
 })
